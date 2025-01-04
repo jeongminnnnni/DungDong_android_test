5204836961d84500c4cff67561a49c583b47f35b
wavicle <spiverain02@gmail.com>
2025-01-05 01:03:51 +0900

    페이지2 작성

--- src/pages/SurveyPage/SurveyPage2.vue
+++ src/pages/SurveyPage/SurveyPage2.vue
@@ -35,29 +35,68 @@ import { onMounted, onUnmounted, ref, computed, watch} from "vue";
 import { useRouter, useRoute } from "vue-router";
 import { routes } from "@/router"
 
+const title = 'MBTI'
+
+const mbti0 = ref(0);
+const mbti1 = ref(0);
+const mbti2 = ref(0);
+const mbti3 = ref(0);
 
 // ----- 라이프 사이클 ----- //
 onMounted(() => {
-
+  setCurrentSurvey()
 });
 
 onUnmounted(() => {
 
 })
 
+// 변경값 확인 및 업데이트
+watch(mbti0, (newValue, oldValue) => {
+  console.log(`--- mbti0 changed from ${oldValue} to ${newValue}`);
+  updateLocalStorage("mbti0", newValue);
+});
+
+watch(mbti1, (newValue, oldValue) => {
+  console.log(`--- mbti1 changed from ${oldValue} to ${newValue}`);
+  updateLocalStorage("mbti1", newValue);
+});
+
+watch(mbti2, (newValue, oldValue) => {
+  console.log(`--- mbti2 changed from ${oldValue} to ${newValue}`);
+  updateLocalStorage("mbti2", newValue);
+});
+
+watch(mbti3, (newValue, oldValue) => {
+  console.log(`--- mbti3 changed from ${oldValue} to ${newValue}`);
+  updateLocalStorage("mbti3", newValue);
+});
+
 // ----- 함수 정의 ----- //
 
-function handleClickTag(value) {
-  const index = selectTag.value.indexOf(value);
+// 최초 로딩
+function setCurrentSurvey() {
+  localStorage.setItem('userProgress', JSON.stringify({ currentStep: 2}));
+  console.log("Updated localStorage userProgress:", localStorage.getItem('userProgress'))
 
-  // 이미 선택된 태그라면 선택을 해제하고, 아니라면 선택 추가
-  if (index !== -1) {
-    selectTag.value.splice(index, 1); // 선택 해제
-  } else {
-    selectTag.value.push(value); // 선택 추가
+  // 저장된 값이 있다면 가져오기
+  const existingSurvey = localStorage.getItem('userSurvey');
+  
+  if (existingSurvey) {
+    const survey = JSON.parse(existingSurvey);
+    mbti0.value = survey.mbti[0] || 0;
+    mbti1.value = survey.mbti[1] || 0;
+    mbti2.value = survey.mbti[2] || 0;
+    mbti3.value = survey.mbti[3] || 0;
   }
+}
 
-  console.log('Selected tags:', selectTag.value);
+// 변경값 로컬스토리지에 저장
+function updateLocalStorage(field, value) {
+  const existingSurvey = JSON.parse(localStorage.getItem("userSurvey")) || {};
+  existingSurvey[field] = value;
+  localStorage.setItem("userSurvey", JSON.stringify(existingSurvey));
+  console.log(`Updated localStorage userSurvey: ${field} = ${value}`);
 }
 
 
