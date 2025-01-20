46a310cfd02ee5e1c8de07d57f28ca1a7765d061
Ebee1205 <spiverain02@gmail.com>
2025-01-21 01:20:41 +0900

    서브타이틀 컴포넌트 추가

---
+++ src/components/SubTitle.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-row>
+        {{ props.title }}
+    </v-row>
+    <v-row>
+        {{ props.desc }}
+    </v-row>
+</template>
+
+<script setup>
+// ----- 선언부 ----- //
+import { onMounted, onUnmounted, ref, computed, watch} from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { routes } from "@/router"
+
+const props = defineProps({
+    title: String,
+    desc: String
+})
+
+
+// ----- 라이프 사이클 ----- //
+onMounted(() => {
+    
+});
+
+onUnmounted(() => {
+
+})
+
+// ----- 함수 정의 ----- //
+
+
+
+
+</script>
+
+<style scoped>
+
+.container-box {
+  padding: 20px;            /* 내부 여백 */
+  width: 100%;              /* 반응형 지원 */
+}
+</style>
