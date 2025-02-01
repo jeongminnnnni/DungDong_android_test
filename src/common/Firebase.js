be55550609d77af532ba32deeca7fb197b9eb85c
Ebee1205 <spiverain02@gmail.com>
2025-02-01 21:06:38 +0900

    파이어베이스 환경변수, 상용키로 변경

--- src/common/Firebase.js
+++ src/common/Firebase.js
@@ -9,14 +9,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAi38KZkhRRruvIBy0Fi3a2BXPLGnO3kDU",
-    authDomain: "findingroommate-37b2a.firebaseapp.com",
-    projectId: "findingroommate-37b2a",
-    storageBucket: "findingroommate-37b2a.firebasestorage.app",
-    messagingSenderId: "799445753557",
-    appId: "1:799445753557:web:e59ffaa67e7e7b17298c5c",
-    measurementId: "G-FMX05RGM9E"
-  };
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+};
 
 // Log: Check if Firebase is already initialized
 if (!getApps().length) {
