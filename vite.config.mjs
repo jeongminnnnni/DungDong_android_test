2153b30f6e16983c13129d3269a2532878a21373
Ebee1205 <spiverain02@gmail.com>
2025-02-02 03:38:44 +0900

    배포준비

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/testBuild/' : '',
+  base: process.env.NODE_ENV === 'production' ? '/DungDong/' : '',
   
   plugins: [
     VueRouter(),
