df7fa1d884343b54c1c17d399e71f04ab70f5047
Ebee1205 <spiverain02@gmail.com>
2025-02-08 12:34:20 +0900

    태그 추가 및 순서 수정

--- src/pages/SurveyPage/SurveyPage7.vue
+++ src/pages/SurveyPage/SurveyPage7.vue
@@ -58,15 +58,16 @@ const selectTag = ref([])
 const tagItem = ref([
   { title: '매트선호', value: '매트선호'},
   { title: '룸메와 거리두기 필요', value: '룸메와 거리두기 필요'},
+  { title: '공부중', value: '공부중'},
   { title: '고요를 즐김', value: '고요를 즐김'},
   { title: '배달음식러버', value: '배달음식러버'},
-  { title: '길고 잦은 통화', value: '길고 잦은 통화'},
+  { title: '잦은 밤샘', value: '잦은 밤샘'},
+  { title: '길거나 잦은 통화', value: '길거나 잦은 통화'},
   { title: '환기 자주 필요', value: '환기 자주 필요'},
   { title: '방에 자주 비움', value: '방을 자주 비움'},
   { title: '추위 많이 탐', value: '추위 많이 탐'},
   { title: '더위 많이 탐', value: '더위 많이 탐'},
   { title: '짐이 많음', value: '짐이 많음'},
-  { title: '잦은 밤샘', value: '잦은 밤샘'},
   { title: '잠귀 밝음', value: '잠귀 밝음'},
 ])
 
